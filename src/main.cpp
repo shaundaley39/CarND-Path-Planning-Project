5d7357e7d5d9baab97de760e75ea0975ccbcf5d5
Shaun Daley <shaundaley39@gmail.com>
2020-06-05 06:59:56 +0000

    feature : stay in lane A first primitive 'planner', which generates way points to keep the vehicle in a target lane at a target speed. The vehicle's position, orientation and velocity are not yet taken into account, so movements are abrupt and constraints are not satisfied. No account is yet taken of other vehicles, speed adjustment for collision avoidance or lane changes. The implementation is dropped into main for now, but this should be separated out and cleaned up once the appropriate structure becomes clear.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -63,12 +64,9 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
         string event = j[0].get<string>();
-        
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -97,8 +95,50 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          // "stay in lane"
+          uint lane = 1;
+          double target_speed = 48.0;
+          int NWP = map_waypoints_x.size(); // should be 181 points
+          vector<double> spline_x;
+          vector<double> spline_y;
+          vector<double> spline_s;
+          int wp_idx = ClosestWaypoint(car_x, car_y, map_waypoints_x, map_waypoints_y);
+          // select waypoints and s values for interpolation
+          for (int i = wp_idx - 1; i <= wp_idx + 2; i++){
+            // integer mod and division are implementation dependent
+            // these are obviously invalid for i < -NWP
+            int i_mod_NWP = (i<0) ? (i+NWP) : (i%NWP);
+            int i_div_NWP = (i<0) ? -1 : (i/NWP);
+            spline_s.push_back(map_waypoints_s[i_mod_NWP] + (i_div_NWP)*6945.554);
+            spline_x.push_back(map_waypoints_x[i_mod_NWP] + (10 - 4 * lane) * map_waypoints_dx[i_mod_NWP]);
+            spline_y.push_back(map_waypoints_y[i_mod_NWP] + (10 - 4 * lane) * map_waypoints_dy[i_mod_NWP]);
+          }
+          // interpolate between these points
+          tk::spline s_x;
+          s_x.set_points(spline_s, spline_x);
+          tk::spline s_y;
+          s_y.set_points(spline_s, spline_y);
+          // generate appropriately spaced points ahead of the vehicle position
+          // this implementation does not yet consider the vehicle's current position!
+          double car_ss = (0!=wp_idx || car_s >= map_waypoints_s[0]) ? car_s : (car_s - 6945.554);
+          std::cout << "car s: " << car_ss <<std::endl;
+          int path_size = 100;
+          double d_ps = (0.020 * 0.44704 * target_speed);
+          for (int i = 0; i < path_size; i++){
+            // this can be dropped
+            double s = car_ss + i*d_ps;
+            next_x_vals.push_back(s_x(s));
+            next_y_vals.push_back(s_y(s));
+          }
 
-
+          // todo: slow down behind a vehicle in this lane
+          // todo: adjust target lane to move around vehicles
+          //       - implement European/ correct driving behaviour, with the vehicle keeping to the right wherever possible
+          //       - implement chaotic & dangerous driving behaviour, with undertaking and lane switches in either direction for the vehicle to move ahead
+          // todo: generate smooth trajectories (satisfying constraints)
+          //       - when the vehicle has the wrong speed
+          //       - when the vehicle is slightly off-spline (e.g. new waypoints)
+          //       - in a lane change
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
